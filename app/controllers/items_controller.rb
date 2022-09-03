2fc12b03489abcbabd4373e18fe51763786f3964
JK <jmjk@JKs-MacBook-Pro.local>
2022-09-03 12:09:16 -0700

    Completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,12 @@
 class ItemsController < ApplicationController
+    def index
+        # render json: Item.all, except: [:created_at, :updated_at]
+        items = Item.all.map { |item| 
+        user = User.find(item.user_id)
+
+        user.as_json
+        item.as_json(include: {user: {except: [:user_id, :created_at, :updated_at]}}, except: [:created_at, :updated_at])}
+
+        render json: items
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+    def show
+        user_with_items = User.find_by(id: params[:id]).as_json(include: {items: {except: [:created_at, :updated_at]}}, except: [:created_at, :updated_at])
+
+        render json: user_with_items
+    end
 end
